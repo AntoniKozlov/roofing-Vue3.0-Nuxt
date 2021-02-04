c7ef05eb3364ef41c2c1ea4ce4aad7027f8c726f
AntoniKozlov <pra.2014@mail.ru>
2021-02-05 00:45:59 +0300

    Added contacts

--- roofing/src/App.vue
+++ roofing/src/App.vue
@@ -8,7 +8,7 @@
 import MainApp from './components/main-app/MainApp.vue'
 
 import Vue from 'vue'
-import { BootstrapVue } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin  } from 'bootstrap-vue'
 import VueScrollTo from 'vue-scrollto'
 import VueScrollIndicator from 'vue-scroll-indicator';
 
@@ -20,7 +20,7 @@ import 'bootstrap';
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-//Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
 
 Vue.use(VueScrollTo, {
   container: "body",
