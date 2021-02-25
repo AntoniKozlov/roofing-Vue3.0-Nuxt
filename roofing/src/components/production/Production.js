6faa36ab63bce93e152ec0a7de34bb589aa31484
AntoniKozlov <pra.2014@mail.ru>
2021-02-26 00:26:46 +0300

    Fixed carousel, changed font-family

--- roofing/src/components/production/Production.js
+++ roofing/src/components/production/Production.js
@@ -1,12 +1,23 @@
 import productionList from '../../consts/productionList';
 import relatedProducts from '../../consts/relatedProducts';
+import { Slider, SliderItem } from "vue-easy-slider";
 
 export default {
     name: 'Production',
+    components: {
+        Slider,
+        SliderItem
+    },
     data() {
         return {
             productionList,
-            relatedProducts
+            relatedProducts,
+            sliderValue: 2
         };
+    },
+    methods: {
+        changeIndex(index) {
+            this.sliderValue = index;
+        }
     }
 }
